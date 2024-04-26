e30919d2e2f146a3eaa48a830f14169985eb9f23
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-26 03:24:29 +0000

    List all bands with Glam rock

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,4 @@
+-- A script that lists all bands with Glam rock as their main style,
+-- ranked by their longevity
+SELECT band_name, (split - formed) AS lifespan
+FROM metal_bands WHERE style = "Glam rock";
