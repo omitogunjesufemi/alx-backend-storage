b5a1f3ce8edd1b891a119277f6290686f652317b
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-27 06:21:51 +0000

    Creates an index idx_name_first on the table names and the first letter of name

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,6 +1,3 @@
 -- Creates an index idx_name_first on the table and the furst
 -- letter of name
-ALTER TABLE names
-ADD COLUMN first_letter CHAR(1) GENERATED ALWAYS AS (LEFT(name, 1)) STORED;
-
-CREATE INDEX idx_name_first ON names (first_letter);
+CREATE INDEX idx_name_first ON names (name, score);
