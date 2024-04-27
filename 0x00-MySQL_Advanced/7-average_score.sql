045e4903df26cc39ce4850c442ce1260774572c2
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-27 05:26:22 +0000

    Creates a stored procedure that computes and store the average score for a student

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,20 @@
+-- Creates a stored procedure that computes and store the
+-- average score for a student
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+
+DECLARE avg_score FLOAT;
+
+SELECT AVG(score) INTO avg_score FROM corrections
+WHERE corrections.user_id = user_id
+GROUP BY corrections.user_id;
+
+UPDATE users SET average_score = avg_score WHERE users.id = user_id;
+
+END//
+
+DELIMITER ;
