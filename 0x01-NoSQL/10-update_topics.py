2c4cc712e04593889b3ac71da552438f18146bbd
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-30 15:56:40 -0700

    Changes all topics of a school documet based on the name

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Python function that changes all topics of a school document based
+on the name
+- Prototype: def update_topics(mongo_collection, name, topics):
+- mongo_collection will be the pymongo collection object
+- name (string) will be the school name to update
+- topics (list of strings) will be the list of topics approached in
+the school
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Python function that changes all topics of a school document
+    based on the name
+    - name (string) will be the school name to update
+    - topics (list of strings) will be the list of topics approached
+    in the school
+    """
+    document = mongo_collection.update_one({"name": name},
+                                           {"$set": {"topics": topics}})
