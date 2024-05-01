a8cf86c1723ad49aaf75517a6964c86995108279
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-02 00:26:23 +0100

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -22,11 +22,44 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        It generates a random key, stores the input data in Redis
+        using the random key.
+
+        Return: the random key generated
+        """
         data_key = str(uuid.uuid4())
         result = self._redis.set(data_key, data)
         if result:
             return data_key
         else:
             return None
+
+    def get(self, key: str, fn=None):
+        """
+        A method that takes a key string arg and an optional
+        Callable argument named fn
+        """
+        result = self._redis.get(key)
+
+        if fn is None:
+            return (result)
+        else:
+            result = fn(result)
+        return (result)
+
+    def get_str(self, data):
+        """
+        This callable is used to convert the data back to string
+        """
+        decoded_data = data.decode("utf-8")
+        return (decoded_data)
+
+    def get_int(self, data):
+        """
+        This callable is used to convert the data back to integer
+        format.
+        """
+        decoded_data = data.decode("utf-8")
+        return (decoded_data)
